09d480e4ec2c1af6d19b9a47e51b96ac5301c2db
suraj <surajmargale777@gmail.com>
2023-04-04 08:55:21 +0530

    Change in logic

--- app/src/main/java/com/suraj/calculator/MainActivity.java
+++ app/src/main/java/com/suraj/calculator/MainActivity.java
@@ -13,15 +13,13 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
     TextView txtNumbersForInput, txtAnswer;
     Button btnClearAll, btnClearOne, btnPercent, btnDivide, btnMultiply, btnMinus, btnPlus, btnCalculate, btnZero, btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine;
+
     String userInput = "";
-
-    int num1 = 0, num2 = 0;
-
+    long number1 = 0;
+    long number2 = 0;
     char sign = '0';
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,320 +61,149 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void setOnClick() {
+    // Create an OnClickListener instance
+    View.OnClickListener buttonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // Get the text of the clicked button
+            String buttonText = ((Button) v).getText().toString();
 
-        btnClearAll.setOnClickListener(new OnClickAction());
-        btnClearOne.setOnClickListener(new OnClickAction());
-        btnCalculate.setOnClickListener(new OnClickAction());
+            // Handle button click based on the button text
+            switch (buttonText) {
+                case "0":
+                    if (userInput!="") {
+                        setInputData("0");
+                    }
+                    break;
+                case "1":
+                    setInputData("1");
+                    break;
+                case "2":
+                    setInputData("2");
+                    break;
+                case "3":
+                    setInputData("3");
+                    break;
+                case "4":
+                    setInputData("4");
+                    break;
+                case "5":
+                    setInputData("5");
+                    break;
+                case "6":
+                    setInputData("6");
+                    break;
+                case "7":
+                    setInputData("7");
+                    break;
+                case "8":
+                    setInputData("8");
+                    break;
+                case "9":
+                    setInputData("9");
+                    break;
+                case "+":
 
-        btnZero.setOnClickListener(new OnClickAction());
-        btnOne.setOnClickListener(new OnClickAction());
-        btnTwo.setOnClickListener(new OnClickAction());
-        btnThree.setOnClickListener(new OnClickAction());
-        btnFour.setOnClickListener(new OnClickAction());
-        btnFive.setOnClickListener(new OnClickAction());
-        btnSix.setOnClickListener(new OnClickAction());
-        btnSeven.setOnClickListener(new OnClickAction());
-        btnEight.setOnClickListener(new OnClickAction());
-        btnNine.setOnClickListener(new OnClickAction());
-        btnPercent.setOnClickListener(new OnClickAction());
-        btnPlus.setOnClickListener(new OnClickAction());
-        btnMinus.setOnClickListener(new OnClickAction());
-        btnDivide.setOnClickListener(new OnClickAction());
-        btnMultiply.setOnClickListener(new OnClickAction());
+                    number1 = Long.parseLong(userInput);
+                    userInput = "";
+                    if (number1 > 0) {
+                        sign = '+';
+                        setSigntData("+");
+                    }
 
+                    break;
+                case "-":
+                    // Handle minus button click
+                    number1 =Long.parseLong(userInput);
+                    userInput = "";
+                    if (number1 > 0) {
+                        sign = '-';
+                        setSigntData("-");
+                    }
+                    break;
+                case "*":
+                    // Handle multiply button click
+                    number1 = Long.parseLong(userInput);
+                    userInput = "";
+                    if (number1 > 0) {
+                        sign = '*';
+                        setSigntData("*");
+                    }
+                    break;
+                case "/":
+                    // Handle divide button click
+                    number1 =Long.parseLong(userInput);
+                    userInput = "";
+                    if (number1 > 0) {
+                        sign = '/';
+                        setSigntData("/");
 
+                    }
+                    break;
+                case "=":
+                    // Handle equals button click
+                    number2 = Long.parseLong(userInput);
+                    userInput = "";
+
+                    switch (sign) {
+                        case '+':
+                            setInputData(String.valueOf(number1 + number2));
+                            break;
+                        case '-':
+                            setInputData(String.valueOf(number1 - number2));
+                            break;
+                        case '*':
+                            setInputData(String.valueOf(number1 * number2));
+                            break;
+                        case '/':
+                            setInputData(String.valueOf(number1 / number2));
+                            break;
+                    }
+
+                    break;
+                case "C":
+                    userInput = "";
+                    number1 = 0;
+                    number2 = 0;
+                    txtNumbersForInput.setText("");
+                    break;
+            }
+        }
+    };
+
+    private void setInputData(String s) {
+
+        userInput = userInput + s;
+        txtNumbersForInput.setText(userInput);
+    }
+    private void setSigntData(String s) {
+
+        txtNumbersForInput.setText(s);
     }
 
-    class OnClickAction implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-
-            // checking which button is clicked and performing the actions
-
-            switch (view.getId()) {
-
-
-                case R.id.btnClearAll:
-                    if (userInput.equals("")) return;
-                    userInput = "";
-                    txtAnswer.setText("");
-
-                    break;
-
-                case R.id.btnClearOne:
-                    if (userInput.equals("")) return;
-                    userInput = userInput.substring(0, userInput.length() - 1);
-                    break;
-
-                case R.id.btnCalculate:
-                    if (userInput.equals("")) return;
-                    //do Calculation
-                    if (checkIsValidInput()) {
-                        doCalculations();
-                    }
-                    break;
-
-
-                case R.id.btnPercent:
-
-                    if (userInput.equals("")) return;
-                    if (!txtAnswer.getText().toString().equals("") && !txtAnswer.getText().toString().contains("Invalid")) {
-                        userInput = txtAnswer.getText().toString();
-                        txtAnswer.setText("");
-                    }
-                    for (int i = 0; i < userInput.length(); i++) {
-                        if (userInput.charAt(i) == '*' || userInput.charAt(i) == '/' || userInput.charAt(i) == '+' || userInput.charAt(i) == '-' || userInput.charAt(i) == '%') {
-                            if (checkIsValidInput()) {
-                                doCalculations2();
-                            }
-                        }
-                    }
-                    userInput = userInput + "%";
-                    break;
-
-
-
-                case R.id.btnDivide:
-                    if (userInput.equals("")) return;
-                    if (!txtAnswer.getText().toString().equals("") && !txtAnswer.getText().toString().contains("Invalid")) {
-                        userInput = txtAnswer.getText().toString();
-                        txtAnswer.setText("");
-                    }
-                    for (int i = 0; i < userInput.length(); i++) {
-                        if (userInput.charAt(i) == '*' || userInput.charAt(i) == '/' || userInput.charAt(i) == '+' || userInput.charAt(i) == '-' || userInput.charAt(i) == '%') {
-                            if (checkIsValidInput()) {
-                                doCalculations2();
-                            }
-                        }
-                    }
-                    userInput = userInput + "/";
-                    break;
-
-
-
-                case R.id.btnMultiply:
-                    if (userInput.equals("")) return;
-                    if (!txtAnswer.getText().toString().equals("") && !txtAnswer.getText().toString().contains("Invalid")) {
-                        userInput = txtAnswer.getText().toString();
-                        txtAnswer.setText("");
-                    }
-                    for (int i = 0; i < userInput.length(); i++) {
-                        if (userInput.charAt(i) == '*' || userInput.charAt(i) == '/' || userInput.charAt(i) == '+' || userInput.charAt(i) == '-' || userInput.charAt(i) == '%') {
-                            if (checkIsValidInput()) {
-                                doCalculations2();
-                            }
-                        }
-                    }
-                    userInput = userInput + "*";
-                    break;
-
-
-
-                case R.id.btnMinus:
-                    if (userInput.equals("")) return;
-                    if (!txtAnswer.getText().toString().equals("") && !txtAnswer.getText().toString().contains("Invalid")) {
-                        userInput = txtAnswer.getText().toString();
-                        txtAnswer.setText("");
-                    }
-                    for (int i = 0; i < userInput.length(); i++) {
-                        if (userInput.charAt(i) == '*' || userInput.charAt(i) == '/' || userInput.charAt(i) == '+' || userInput.charAt(i) == '-' || userInput.charAt(i) == '%') {
-                            if (checkIsValidInput()) {
-                                doCalculations2();
-                            }
-                        }
-                    }
-
-                    userInput = userInput + "-";
-                    break;
-
-
-
-                case R.id.btnPlus:
-                    if (userInput.equals("")) return;
-
-                    if (!txtAnswer.getText().toString().equals("") && !txtAnswer.getText().toString().contains("Invalid")) {
-                        userInput = txtAnswer.getText().toString();
-                        txtAnswer.setText("");
-                    }
-
-                    for (int i = 0; i < userInput.length(); i++) {
-                        if (userInput.charAt(i) == '*' || userInput.charAt(i) == '/' || userInput.charAt(i) == '+' || userInput.charAt(i) == '-' || userInput.charAt(i) == '%') {
-                            if (checkIsValidInput()) {
-                                doCalculations2();
-                            }
-                        }
-                    }
-                    userInput = userInput + "+";
-                    break;
-
-
-
-                case R.id.btnZero:
-                    if (!userInput.equals("")) {
-                        userInput = userInput + "0";
-                    }
-                    break;
-
-                case R.id.btnOne:
-                    userInput = userInput + "1";
-                    break;
-
-                case R.id.btnTwo:
-                    userInput = userInput + "2";
-                    break;
-
-                case R.id.btnThree:
-                    userInput = userInput + "3";
-                    break;
-
-                case R.id.btnFour:
-                    userInput = userInput + "4";
-                    break;
-
-                case R.id.btnFive:
-                    userInput = userInput + "5";
-                    break;
-
-                case R.id.btnSix:
-                    userInput = userInput + "6";
-                    break;
-
-                case R.id.btnSeven:
-                    userInput = userInput + "7";
-                    break;
-
-                case R.id.btnEight:
-                    userInput = userInput + "8";
-                    break;
-
-                case R.id.btnNine:
-                    userInput = userInput + "9";
-                    break;
-
-                default:
-                    Toast.makeText(MainActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
-            }
-
-            txtNumbersForInput.setText(userInput);
-        }
-
-
-
-
-
-
-
-        private void doCalculations() {
-            assignValues();
-            switch (sign) {
-                case '+':
-                    txtAnswer.setText(String.valueOf(num1 + num2));
-                    break;
-                case '-':
-                    txtAnswer.setText(String.valueOf(num1 - num2));
-                    break;
-                case '*':
-                    txtAnswer.setText(String.valueOf(num1 * num2));
-                    break;
-                case '/':
-                    if (num1 == 0 || num2 == 0) {
-                        txtAnswer.setText(String.valueOf(0));
-                        return;
-                    }
-                    txtAnswer.setText(String.valueOf(num1 / num2));
-                    break;
-                case '%':
-                    if (num1 == 0 || num2 == 0) {
-                        txtAnswer.setText(String.valueOf(0));
-                        return;
-                    }
-                    txtAnswer.setText(String.valueOf(num1 % num2));
-                    break;
-            }
-
-        }
-
-        private void doCalculations2() {
-            assignValues();
-            switch (sign) {
-                case '+':
-                    userInput = String.valueOf(num1 + num2);
-                    break;
-                case '-':
-
-                    userInput = String.valueOf(num1 - num2);
-                    break;
-                case '*':
-
-                    userInput = String.valueOf(num1 * num2);
-                    break;
-                case '/':
-                    if (num1 == 0 || num2 == 0) {
-
-                        userInput = String.valueOf(num1 / num2);
-                        return;
-                    }
-
-                    userInput = String.valueOf(num1 / num2);
-                    break;
-                case '%':
-                    if (num1 == 0 || num2 == 0) {
-                        txtAnswer.setText(String.valueOf(0));
-                        userInput = String.valueOf(0);
-                        return;
-                    }
-                    userInput = String.valueOf(num1 % num2);
-                    break;
-            }
-            txtNumbersForInput.setText(userInput);
-        }
-
-        private void assignValues() {
-            for (int i = 0; i < userInput.length(); i++) {
-                if (userInput.charAt(i) == '*' || userInput.charAt(i) == '/' || userInput.charAt(i) == '+' || userInput.charAt(i) == '-' || userInput.charAt(i) == '%') {
-
-                    // check if user only enters single number and sign
-                    if (i == userInput.length() - 1) {
-                        txtAnswer.setText("Invalid!!");
-                        return;
-                    }
-
-                    num1 = Integer.parseInt(userInput.substring(0, i));
-                    num2 = Integer.parseInt(userInput.substring(i + 1));
-                    sign = userInput.charAt(i);
-
-                }
-            }
-        }
-
-
-
-        private boolean checkIsValidInput() {
-
-            int count = 0;
-            for (int i = 0; i < userInput.length(); i++) {
-                if (userInput.charAt(i) == '*' || userInput.charAt(i) == '/' || userInput.charAt(i) == '+' || userInput.charAt(i) == '-' || userInput.charAt(i) == '%') {
-                    count++;
-                }
-            }
-
-
-            if (count > 1) {
-                Toast.makeText(MainActivity.this, "Please enter valid input", Toast.LENGTH_SHORT).show();
-                txtAnswer.setText("Invalid!!");
-                return false;
-            }
-            return true;
-
-        }
-
+    private void setOnClick() {
+
+        btnClearAll.setOnClickListener(buttonClickListener);
+        btnCalculate.setOnClickListener(buttonClickListener);
+
+        btnZero.setOnClickListener(buttonClickListener);
+        btnOne.setOnClickListener(buttonClickListener);
+        btnTwo.setOnClickListener(buttonClickListener);
+        btnThree.setOnClickListener(buttonClickListener);
+        btnFour.setOnClickListener(buttonClickListener);
+        btnFive.setOnClickListener(buttonClickListener);
+        btnSix.setOnClickListener(buttonClickListener);
+        btnSeven.setOnClickListener(buttonClickListener);
+        btnEight.setOnClickListener(buttonClickListener);
+        btnNine.setOnClickListener(buttonClickListener);
+
+        btnPlus.setOnClickListener(buttonClickListener);
+        btnMinus.setOnClickListener(buttonClickListener);
+        btnDivide.setOnClickListener(buttonClickListener);
+        btnMultiply.setOnClickListener(buttonClickListener);
 
     }
 }
-
-
-
 
 
 
